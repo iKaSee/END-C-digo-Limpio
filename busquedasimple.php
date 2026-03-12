c5bcc5a97cf5b078fad37b1f2da202c0a1e042b9
xXRubio10Xx <rubiosalvadormarti@gmail.com>
2026-03-12 19:06:13 +0100

    Compress search file and render search result in the index

--- busquedasimple.php
+++ busquedasimple.php
@@ -1,8 +1,11 @@
 <?php
+
 $expressio = $_GET["expr"];
-require_once("conexion_pdo.php");
+
+require_once("Conexion_MySQL.php");
 $dbClass = new Database();
 $db = $dbClass->getConnection();
+
 $dbTabla='noticiasFTInnodb';
 
 $consulta = "SELECT * FROM $dbTabla WHERE MATCH(titulo, cuerpo) AGAINST (:e)"; 
@@ -13,20 +16,12 @@ $result->execute(array(":e" => $expressio));
 $total = $result->rowCount();
 if ($total>0){ //Tenim resultats per la cerca
 	print "<h2>Tenemos $total de resultados para <b>$expressio</b> en la bbdd </h2>\n";
-	//Fi Processament
-	
-	//Processament + output
-	if (!$result){ 
-		print "<p> Error en la consulta. </p>\n";
-	}else{ 
-		print "<ol>";
-		foreach( $result as $valor){
-			print "<li>".$valor["titulo"]."</li>";
-		} 
-		print "</ol>";
-	}
-	//Fi Processament
-}else{// No hi ha resultats per la cerca
+	print "<ol>";
+	foreach( $result as $valor){
+		print "<li>".$valor["titulo"]."</li>";
+	} 
+	print "</ol>";
+}else{ // No hi ha resultats per la cerca
 	print "<h2>No hay resultados para <b>$expressio</b> en la bbdd </h2>\n";
 }
 //Cerramos conexión
--- index.php
+++ index.php
@@ -4,11 +4,12 @@
 <meta charset="utf-8">
 <title>Busqueda simple</title>
 </head>
-<body>
+<body">
 <h1>Cercador</h1>
-<form method="get" action="busquedasimple.php">
+<form method="get" action="busquedasimple.php" target="resultados">
 <label>Cerca<input type="text" name="expr"></label>
 <input type="submit" value="cerca!">
 </form>
+<iframe name="resultados" width="100%" height="100%" frameborder="0"></iframe>
 </body>
 </html>
